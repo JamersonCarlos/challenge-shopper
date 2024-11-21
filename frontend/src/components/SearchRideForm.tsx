9115b1267f869e09bb7a4e3c05e57c3c52d7faae
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-11-21 11:05:32 -0300

    Formulário para captura do endereço de origem e de destino
    Foi incluído o preenchimento automático dos endereços solicitados, utilizando a API do Google Maps.

---
+++ frontend/src/components/SearchRideForm.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./SearchRideForm.css";
+import { FaIdCard, FaMapMarkerAlt, FaLocationArrow } from "react-icons/fa";
+import { useJsApiLoader, Autocomplete } from "@react-google-maps/api";
+
+const SearchRideForm = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyAMqYayPsqD8jqBw_dL-aclx-_y2UCbClA",
+    libraries: ["places"],
+    language: "pt",
+  });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <span>CPF</span>
+        <div className="input-container">
+          <input
+            type="text"
+            name="cpf"
+            placeholder="Ex: 12031405123"
+            maxLength={11}
+          />
+          <FaIdCard className="icon-right" /> {/* Ícone React Icons */}
+        </div>
+      </label>
+
+      <label>
+        <span>Origem</span>
+        <div className="input-container">
+          <Autocomplete>
+            <input type="text" name="origem" placeholder="Endereço de origem" />
+          </Autocomplete>
+          <FaMapMarkerAlt className="icon-right" /> {/* Ícone React Icons */}
+        </div>
+      </label>
+
+      <label>
+        <span>Destino</span>
+        <div className="input-container">
+          <Autocomplete>
+            <input
+              type="text"
+              name="destino"
+              placeholder="Endereço do destino"
+            />
+          </Autocomplete>
+          <FaLocationArrow className="icon-right" /> {/* Ícone React Icons */}
+        </div>
+      </label>
+
+      <input type="submit" value="Buscar" />
+    </form>
+  );
+};
+
+export default SearchRideForm;
