80e353d423efa74f9781895f09d6131362f189fc
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-11-21 11:09:25 -0300

    Incluindo rotas e fontes externas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//Import components 
+import NavBar from "./components/NavBar";
+
+//Import Pages
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
