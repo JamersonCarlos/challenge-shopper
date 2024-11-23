93ff913f3194d817948a5eb76285609bdda004fb
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-11-23 10:07:29 -0300

    função para encontrar a latitude e longitude de um determinado endereço

---
+++ backend/utils/getLongitudeAndLatitude.ts
@@ -0,0 +1,27 @@
+//Google Maps
+import { Client } from "@googlemaps/google-maps-services-js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const getLatLng = async (address: string, client: Client ) => {
+    try {
+      const geocodeResponse = await client.geocode({
+        params: {
+          address,
+          key: process.env.GOOGLE_API_KEY!,
+        },
+      });
+      
+      //Se não retornar nada, o endereço é invalido!
+      if (geocodeResponse.data.results.length === 0) {
+        throw new Error("Unable to geocode address");
+      }
+  
+      const { lat, lng } = geocodeResponse.data.results[0].geometry.location;
+      return { latitude: lat, longitude: lng };
+    } catch (error) {
+      throw new Error(`Error fetching latitude and longitude: ${(error instanceof Error) ? error.message : ""}`);
+    }
+  };
+
+export default getLatLng; 
