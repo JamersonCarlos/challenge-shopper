8e8cb0cfb421dd3e3fc4d93b26d0bb044e29b3b4
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-11-23 18:39:59 -0300

    Serviço responsável por buscar os motoristas disponiveis

---
+++ frontend/src/services/driverService.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosError } from "axios";
+
+//Functions import
+import { isErrorInvalidAddress } from "../interfaces/resultRide.interface";
+
+//Interface Error
+import { ErrorInvalidAddress } from "../interfaces/resultRide.interface";
+
+const url = "http://localhost:8080/";
+
+export const searchDriver = async (
+  id: string,
+  origin: string,
+  destination: string
+) => {
+  try {
+    const response = await axios.post(
+      `${url}ride/estimate`,
+      {
+        id,
+        origin,
+        destination,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response?.data) {
+        const dataError: ErrorInvalidAddress | Object =
+          axiosError.response?.data;
+        return dataError;
+      }
+    }
+  }
+};
