3be80189a02aa625cd7f7dd97d629283bd54970e
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-11-23 18:39:39 -0300

    Carregamento da rota
    Utilizei uma função callback passada para o formulário de captura dos dados, quando o formulário é submetido, essa função modifica o estado de um variável, ativando assim o useEffect. Que executa o código de encontrar a melhor rota, e depois envia para o mapa e o centraliza de acordo com a origem.

--- frontend/src/pages/NovaCorrida.tsx
+++ frontend/src/pages/NovaCorrida.tsx
@@ -1,20 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./NovaCorrida.css";
+
+//Interfaces
+import { ResultRides } from "../interfaces/resultRide.interface";
+
+//Components
 import SearchRideForm from "../components/SearchRideForm";
-import { useState } from "react";
 
 //Google Maps
-import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api";
-const center = { lat: -23.541596480067913, lng: -46.629363693411726 };
+import {
+  useJsApiLoader,
+  GoogleMap,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
 
 const NovaCorrida = () => {
+  const [mapa, setMapa] = useState<google.maps.Map>();
+  const [center, setCenter] = useState({
+    lat: -23.541596480067913,
+    lng: -46.629363693411726,
+  });
+  const [drivers, setDrivers] = useState<ResultRides>();
+  const [directionsResponse, setDirectionsResponse] = useState<
+    google.maps.DirectionsResult | undefined
+  >();
+
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: "AIzaSyAMqYayPsqD8jqBw_dL-aclx-_y2UCbClA",
     libraries: ["places"],
     language: "pt",
   });
 
-  const [mapa, setMapa] = useState<google.maps.Map>();
+  useEffect(() => {
+    if (isLoaded) {
+      const directionsService = new window.google.maps.DirectionsService();
+      setCenter({
+        lat: Number(drivers?.origin.latitude),
+        lng: Number(drivers?.origin.longitude),
+      });
+      // Solicitar a rota entre a origem e o destino
+      const origin = {
+        lat: Number(drivers?.origin.latitude),
+        lng: Number(drivers?.origin.longitude),
+      };
+      const destination = {
+        lat: Number(drivers?.destination.latitude),
+        lng: Number(drivers?.destination.longitude),
+      };
+      directionsService.route(
+        {
+          origin,
+          destination,
+          travelMode: window.google.maps.TravelMode.DRIVING,
+        },
+        (result, status) => {
+          console.log("Status da rota:", status);
+          if (status === "OK" && result) {
+            console.log("Rota encontrada:", result);
+            setDirectionsResponse(result);
+          } else {
+            console.error("Erro ao obter a rota:", status);
+          }
+        }
+      );
+    }
+  }, [drivers]);
 
   if (!isLoaded) {
     return <div>Carregando mapa...</div>; // Renderize algo enquanto o mapa carrega
@@ -23,9 +73,7 @@ const NovaCorrida = () => {
   return (
     <div className="search-ride">
       <div className="search-ride-container">
-        <div className="search-ride-form">
-          <SearchRideForm></SearchRideForm>
-        </div>
+        <SearchRideForm onResult={setDrivers}></SearchRideForm>
         <GoogleMap
           center={center}
           zoom={14}
@@ -42,7 +90,11 @@ const NovaCorrida = () => {
           }}
           onLoad={(map) => setMapa(map)}
         >
-          <Marker position={center}></Marker>
+          {directionsResponse && (
+            <DirectionsRenderer
+              directions={directionsResponse}
+            ></DirectionsRenderer>
+          )}
         </GoogleMap>
       </div>
     </div>
