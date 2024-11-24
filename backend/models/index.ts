2b38d9abd0e2eb1ef7f54bbdd2a7fdcd37461ec9
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-11-24 14:50:29 -0300

    inicializando modelo no banco

--- backend/models/index.ts
+++ backend/models/index.ts
@@ -1,10 +1,12 @@
 import { sequelize } from "../config/sequelize";
 import { Driver } from "./driver.model";
 import { Assessment } from "./assessment.model";
+import { Trip } from "./trip.model";
 
 const db = {
   Driver,
   Assessment,
+  Trip,
   sequelize,
 };
 
