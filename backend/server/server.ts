3b28886968f116b931b4c99143fc8f0d4e6ec789
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-11-24 11:45:19 -0300

    Incluindo a utilização de cors
    Permite que requisições sejam feitas por um domínio diferente do servidor.

--- backend/server/server.ts
+++ backend/server/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 const routers = require("../controllers");
-
+const cors = require("cors");
 const server = express();
+
+server.use(cors());
 
 // Roteamento da aplicação
 server.use(express.json());
