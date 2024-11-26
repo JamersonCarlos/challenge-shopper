31ea4af1f92ffe2423238b8af769e53024fd9795
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-11-26 02:45:05 -0300

    Update NovaCorrida.tsx

--- frontend/src/pages/NovaCorrida.tsx
+++ frontend/src/pages/NovaCorrida.tsx
@@ -29,7 +29,9 @@ const NovaCorrida = () => {
   >();
 
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: "AIzaSyAMqYayPsqD8jqBw_dL-aclx-_y2UCbClA",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY
+      ? process.env.REACT_APP_GOOGLE_API_KEY
+      : "",
     libraries: ["places"],
     language: "pt",
   });
