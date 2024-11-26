10431076e291067ce8d0d05647f9aaf87bd175f0
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-11-26 12:28:20 -0300

    definindo rota

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import Home from "./pages/Home";
 import NovaCorrida from "./pages/NovaCorrida";
 import Trips from "./pages/Trips";
 import ChooseDriver from "./pages/ChooseDriver";
+import Motoristas from "./pages/Motoristas";
 
 function App() {
   return (
@@ -22,9 +23,13 @@ function App() {
             path="/solicitar-corrida"
             element={<NovaCorrida></NovaCorrida>}
           ></Route>
-          <Route path="/motoristas-disponiveis" element={<ChooseDriver></ChooseDriver>}></Route>
+          <Route
+            path="/motoristas-disponiveis"
+            element={<ChooseDriver></ChooseDriver>}
+          ></Route>
           {/* <Route path="/motoristas" element={<Drivers/>} ></Route> */}
           <Route path="/viagens" element={<Trips></Trips>}></Route>
+          <Route path="/motoristas" element={<Motoristas></Motoristas>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
