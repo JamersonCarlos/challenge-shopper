48ed1c8a27d4ed509502c0a387c89fe12d2608be
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-11-26 19:08:08 -0300

    definindo a rota para o endpoint criado

--- backend/controllers/index.ts
+++ backend/controllers/index.ts
@@ -2,8 +2,10 @@ import express from 'express';
 const router = express.Router();
 
 const rideRouter: NodeRequire = require('./ride.controller');
+const driverRouter: NodeRequire = require('./drivers.controller');
 
 router.use('/ride', rideRouter);
+router.use('/drivers', driverRouter);
 
 module.exports = router;
 
