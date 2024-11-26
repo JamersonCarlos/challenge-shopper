21a0e6c7cc3786666591428d82ff711fe47bff5f
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-11-26 19:13:45 -0300

    Update index.ts

--- backend/controllers/index.ts
+++ backend/controllers/index.ts
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const rideRouter: NodeRequire = require('./ride.controller');
 const driverRouter: NodeRequire = require('./drivers.controller');
+const tripRouter: NodeRequire  = require('./trip.controller')
 
 router.use('/ride', rideRouter);
 router.use('/drivers', driverRouter);
+router.use('/trips', tripRouter);
 
 module.exports = router;
 
